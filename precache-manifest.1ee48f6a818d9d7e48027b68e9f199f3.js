6643849c57faa66f129489c0fa7bdf8195ce9345
rpassalacqua <rjgp1998@hotmail.com>
2019-10-26 11:23:48 +0100

    Updates

---
+++ precache-manifest.1ee48f6a818d9d7e48027b68e9f199f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458e5eb6dbd7fff6b9f4932c1e51ffd4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95aa06d1f3985a730aec",
+    "url": "/monsters-rolodex/static/css/main.9b4d144e.chunk.css"
+  },
+  {
+    "revision": "a6e7282b7dd326a54618",
+    "url": "/monsters-rolodex/static/js/2.2976affa.chunk.js"
+  },
+  {
+    "revision": "95aa06d1f3985a730aec",
+    "url": "/monsters-rolodex/static/js/main.1ce12420.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
